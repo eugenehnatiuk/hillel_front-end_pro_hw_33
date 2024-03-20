520e0e521916c6efc2457d08831ee05fad7708b8
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2024-03-20 14:45:17 +0200

    minor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import { HashRouter } from 'react-router-dom';
+
 
 const mainEl = document.getElementById('root');
 const container = ReactDOM.createRoot(mainEl);
